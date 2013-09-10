8c4af1aab126249f293417a05dde939ba16ae51f
andet12 <andersvaffel@hotmail.com>
2013-09-10 19:18:10 +0200

    javascript for å gjøre html siden dynamisk

---
+++ proglangs.js
@@ -0,0 +1,5 @@
+function ToOutbox(selectobj){
+ var e_proglangs = document.getElementById("proglangs");
+ var e_output = document.getElementById("output");
+ e_output.innerHTML = "Du har valgt programmeringsspraket " + e_proglangs.options[selectobj.selectedIndex].text;
+}
