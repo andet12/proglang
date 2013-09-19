91a911bc797ba8bda1cfdbd71e6aa74f8149f40b
andet12 <andersvaffel@hotmail.com>
2013-09-19 18:55:00 +0200

    la til popup boxer

--- proglangs.js
+++ proglangs.js
@@ -1,5 +1,9 @@
 function ToOutbox(selectobj){
- var e_proglangs = document.getElementById("proglangs");
- var e_output = document.getElementById("output");
- e_output.innerHTML = "Du har valgt programmeringsspraket " + e_proglangs.options[selectobj.selectedIndex].text;
+	var e_proglangs = document.getElementById("proglangs");
+	var e_output = document.getElementById("output");
+	e_output.innerHTML = "Du har valgt programmeringsspraket " + e_proglangs.options[selectobj.selectedIndex].text;
+}
+
+function advarsel() {
+	alert("Denne delen kommer snart!");
 }
