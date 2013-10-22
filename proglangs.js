51a511a010d04293637f319fa3f8de73fd957f8b
andet12 <andersvaffel@hotmail.com>
2013-10-22 19:19:29 +0200

    Puttet i mapper

--- proglangs.js
+++
@@ -1,9 +0,0 @@
-function ToOutbox(selectobj){
-	var e_proglangs = document.getElementById("proglangs");
-	var e_output = document.getElementById("output");
-	e_output.innerHTML = "Du har valgt programmeringsspraket " + e_proglangs.options[selectobj.selectedIndex].text;
-}
-
-function advarsel() {
-	alert("Denne delen kommer snart!");
-}
